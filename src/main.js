8d0696094bdf14bd98b8c319b93ae57065e81172
Vitalya-kon <vitalya.kon123@gmail.com>
2024-06-29 21:12:14 +0500

    second1

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import './assets/main.css'
-import 'tailwindcss/tailwind.css'
+
 
 import { createApp } from 'vue'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 
 import App from './App.vue'
 
+import './assets/main.css'
 import 'aos/dist/aos.css'
+import 'tailwindcss/tailwind.css'
 
 const app = createApp(App);
 
